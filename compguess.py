66ccffc7ea8b683ac8be0af14415f115dede033d
Shubham Goel <33158343+shubhamgoel01@users.noreply.github.com>
2021-01-16 10:18:17 +0530

    Add files via upload

---
+++ compguess.py
@@ -0,0 +1,15 @@
+import random
+
+def guesscomp(x):
+    random_number = random.randint(1,x)
+    guess = 0
+    while(guess != random_number):
+        guess = int(input(f'Guess a Number between (1,{x}): '))
+        if(guess < random_number):
+            print("SorrY !! Your guess - LOW")
+        elif guess >random_number:
+            print("SorrY !! Your Guess - High")
+
+    print(f'Winner Winner , Right Guess !! , Number : {random_number}')
+
+guesscomp(10)
